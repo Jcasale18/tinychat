811d68a5909e795d4cb3f25539cd098005a0b63f
Joseph Casale <Jrccasale@gmail.com>
2021-04-19 11:47:40 -0400

    finishing EvensAndOdds, and making Timer use String.format

--- hw1/Timer.java
+++ hw1/Timer.java
@@ -1,9 +1,7 @@
 package hw1;
-
 import java.util.Vector;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 /**
  * This demonstrates that Vector is slower than ArrayList; I believe this is the case
@@ -11,30 +9,27 @@ import java.util.Vector;
  * it to be a little slower.
  */
 public class Timer {
-    public static long timeFill(String name, List<Integer> buffer, long numberOfValues){
+    public static void timeFill(String name, List<Integer> buffer, long numberOfValues){
         long start = System.nanoTime();
         for(int i = 0; i < numberOfValues; i++){
             buffer.add(i);
         }
         long end = System.nanoTime();
 
-        return (end - start);
+        long time = (end - start);
+        System.out.println(String.format("Filled %s with %,d values in %,d nanoseconds.",name, buffer.size(), time));
     }
     public static void main(String[] args) {
         List<Integer> buffer1 = new ArrayList<>();
         List<Integer> buffer2 = new Vector<>();
         Thread arraylistfill = new Thread( () -> {
             
-            long time = timeFill("ArrayList", buffer1, 100000000);
-            System.out.println("Filled ArrayList with " + buffer1.size() + " values in " + time + " nanoseconds.");
-            
+            timeFill("ArrayList", buffer1, 100000000);            
             
         });
         Thread vectorlistfill = new Thread( () -> {
             
-            long time = timeFill("Vector", buffer2, 100000000);
-            System.out.println("Filled vector with " + buffer2.size() + " values in " + time + " nanoseconds.");
-
+            timeFill("Vector", buffer2, 100000000);
         });
 
         arraylistfill.start();
