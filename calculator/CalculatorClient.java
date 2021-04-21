ac10a96a6e4f4123a35ccecbb8f124e374308b55
Joseph Casale <Jrccasale@gmail.com>
2021-04-21 00:39:34 -0400

    fixing issue with working on multiple clients -- assuring the calculator server works the same as Calculator

--- calculator/CalculatorClient.java
+++ calculator/CalculatorClient.java
@@ -1,3 +1,4 @@
+//Joseph Casale
 package calculator;
 
 import java.io.IOException;
@@ -6,25 +7,52 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
 
+/**
+ * Calculator Client connects to Calculator Server being run by localhost on port 12400.
+ */
 public class CalculatorClient {
     public static void main(String[] args) throws IOException {
+        // sestup client
         DatagramSocket client = new DatagramSocket();
+
+        //get operation from user (first run will be op operator op, others will be operator op form)
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter math operation (or nothing to exit): ");
-
+        String op = "";
         while(true){
-            String op = scanner.nextLine().strip();
+            try {
+                if (Double.isNaN (Double.parseDouble(op))) {
+                    throw new NumberFormatException ();
+                }
+            } catch (NumberFormatException nfe) { 
+                // Didn't get a numberic response, throw away the result
+                if(!op.equals("")){//If server sends something like "error bad request", we want to get rid of that first and print a new line, but we don't want to print that new line at the very start when op is "".
+                    op = "";
+                    System.out.println ();
+                }
+            }
+
+
+
+            op = op + scanner.nextLine().strip();//adds the inputted operation to the op string so that it works for the first run starting at empty string, and also works for second runs with the incoming response
             if(op.equals("")){
                 break;
             }
             InetAddress target = InetAddress.getByName("localhost");
             DatagramPacket outgoing = new DatagramPacket(op.getBytes(), op.length(), target, 12400);
+
+            //sends operation always of form (operand operator operand) to server
             client.send(outgoing);
             byte[] buffer = new byte[1024];
+            //recieves a result
             DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
             client.receive(incoming);
+            //turns result to string
             String result = new String(incoming.getData(), 0, incoming.getLength());
             System.out.print(result);
+            //make a new string for the result -- makes more readable for me. A single char array shouldn't hurt too much.
+            op = new String(incoming.getData(), 0, incoming.getLength());
+
         }
         scanner.close();
         client.close();
--- calculator/CalculatorServer.java
+++ calculator/CalculatorServer.java
@@ -1,3 +1,4 @@
+//Joseph Casale
 package calculator;
 
 import java.io.IOException;
@@ -6,9 +7,13 @@ import java.net.DatagramSocket;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Server takes "operand operation operand" requests, calculates a result, and sends it back.
+ */
 public class CalculatorServer{
 
     public static void main(String[] args) throws IOException {
+        //make the calculator
         List<BinaryOperation> OPERATIONS = new ArrayList<>(7);
         OPERATIONS.add (new Addition());
         OPERATIONS.add (new Subtraction());
@@ -16,7 +21,9 @@ public class CalculatorServer{
         OPERATIONS.add (new Division());
         OPERATIONS.add (new FloorDivision());
         OPERATIONS.add (new Exponent());
-        Calculator calculator = new Calculator(OPERATIONS);        
+        Calculator calculator = new Calculator(OPERATIONS);  
+        
+        //setup server and listen for an expression
         DatagramSocket server = new DatagramSocket(12400);
         byte[] buffer = new byte[1024];
         DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
@@ -24,7 +31,7 @@ public class CalculatorServer{
         String expression = new String(incoming.getData(), 0, incoming.getLength());
 
 
-
+        //begin loop to maintain calculation until just a space is sent
         while (!expression.equals(" ")) {
             String[] tokens = expression.strip().split(" ");
     
@@ -34,46 +41,38 @@ public class CalculatorServer{
                 result = "error bad request ";
             } 
             else {
-                    // Parse the components
+                // Parse the components
                 try {
                     float operand1 = Float.parseFloat(tokens[0]);
                     float operand2 = Float.parseFloat(tokens[2]);
-                        // Get the result as a String
+                    // Get the result as a String
                     result = "" + calculator.calculate(tokens[1], operand1, operand2) + " ";
                 } catch (Exception e) {
-                        // Uh-oh, something bad happened, record it as the result
+                    // Uh-oh, something bad happened, record it as the result
                     result = "error " + e.getLocalizedMessage() + " ";
                 }
             }
     
                     
-                /**
-                 * If the response was a number, use that number as the first value
-                 * in a new binary operation. If it was anything else, expect
-                 * a new 3 piece binary operation.
-                 */
+            /**
+             * If the response was a number, use that number as the first value
+             * in a new binary operation. If it was anything else, expect
+             * a new 3 piece binary operation.
+            */
+
+            //creating a copy of the result to send after internally updating result string to work with
             String tosend = String.valueOf(result);
-            try {
-                if (Double.isNaN (Double.parseDouble(result.strip()))) {
-                    throw new NumberFormatException ();
-                }
-            } catch (NumberFormatException nfe) { 
-                    // Didn't get a numberic response, throw away the result
-                result = "";
-            }
+
                 
-                // Let the user know the result.
+            // Let the user know the result.
                 
             DatagramPacket packet = new DatagramPacket(tosend.getBytes(), tosend.length(), incoming.getAddress(), incoming.getPort());
             server.send(packet);
-            System.out.print (result + " ");
 
 
-            server.receive(incoming);                
-                // Add the new operation to the old result to form 
-                // a complete operation. I.E. result was 5, user entered
-                // * 7, the new input would be "5 * 7".
-            expression = result +  new String(incoming.getData(), 0, incoming.getLength());
+            //get new expression for re-iteration and possible exit
+            server.receive(incoming);            
+            expression = new String(incoming.getData(), 0, incoming.getLength());
 
         }
         server.close();
